5242d7093acbb8545f93d9293ba0f09b8dbe61fb
wolfjfly <104739255+wolfjfly@users.noreply.github.com>
2022-10-29 08:13:13 -0500

    worked on genrateMarkdown

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const inquirer=require('inquirer');
 const fs=require('fs');
 const generateMarkdown=require('./utils/generateMarkdown');
-const ui = new inquirer.ui.BottomBar();
 
 // TODO: Create an array of questions for user input
 const questions = [
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,61 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  [![License Badge](https://img.shields.io/badge/license-${data.license}-success?style=plastic)](https://choosealicense.com/licenses/${data.license.toLowerCase()}/)
 
-`;
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [Contributing](#contributing)
+  4. [Credits](#credits)
+  4. [Tests](#tests)
+  5. [License](#license)
+  6. [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  <p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+  ## Usage
+  ${data.usage}
+  <p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+  ## Contributing
+  ${data.contributing}
+  <p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+  ## Tests
+  ${data.tests}
+  <p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+  ## License
+  Copyright (c) [${data.username}](https://github.com/${data.username}). All rights reserved. 
+  \nLicensed under the [${data.license} license](https://choosealicense.com/licenses/${data.license.toLowerCase()}/).
+  <p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+  ## Questions
+  Feel free to contact me with questions or comments @:
+  - GitHub: [${data.username}](https://github.com/${data.username})
+  - Email: [${data.email}](mailto:${data.email})
+  <p align="right">(<a href="#readme-top">back to top</a>)</p>
+  `;
 }
 
 module.exports = generateMarkdown;
