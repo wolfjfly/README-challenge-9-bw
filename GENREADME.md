73347c79f69a54ee9be417073623eb1b320e04ac
wolfjfly <104739255+wolfjfly@users.noreply.github.com>
2022-11-01 22:37:13 -0500

    Final push

---
+++ GENREADME.md
@@ -0,0 +1,50 @@
+
+  
+  # Professional README Generator
+  
+  ## Description
+  AS A developer I WANT a README generator SO THAT I can quickly create a professional README for a new project
+
+  [![License Badge](https://img.shields.io/badge/license-MIT-success?style=plastic)](https://choosealicense.com/licenses/mit/)
+  
+  
+  ## Table of Contents
+  1. [Installation](#installation)
+  2. [Usage](#usage)
+  3. [Contributing](#contributing)
+  4. [Credits](#credits)
+  5. [Tests](#tests)
+  6. [License](#license)
+  7. [Questions](#questions)
+  
+  ## Installation
+  Install node v14 or greater, Install inquirer 8.2.4
+  
+
+  ## Usage
+  In the terminal type ‘node index.js’
+  
+
+  ## Contributing
+  To contribute please follow the link and read the contributing guide. https://github.com/github/docs/blob/dd9f70f9e7edb76e79fc43dee8fb3b8b8dfd1d20/CONTRIBUTING.md
+  
+
+  ## Credits
+  Google,  https://www.npmjs.com/package/inquirer,  https://stackoverflow.com/
+
+  ## Tests
+  N/A
+  
+
+  ## License
+  Copycenter (c) [wolfjfly](https://github.com/wolfjfly). All centers reserved. 
+  
+Licensed under the [MIT license](https://choosealicense.com/licenses/mit/)
+  
+
+  ## Questions
+  Feel free to contact me with questions or comments @:
+  - GitHub: [wolfjfly](https://github.com/wolfjfly)
+  - Email: [benjamin_wlf@yahoo.com](mailto:benjamin_wlf@yahoo.com)
+  
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,9 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-// function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-// function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-
-// ![GitHub](https://img.shields.io/github/license/wolfjfly/challenge-9-bw?style=plastic)
 function generateMarkdown(data) {
-  return `<p id="bringToTop"></p>
+  return `
   
   # ${data.title}
   
-  # ${data.title}
-
   ## Description
   ${data.description}
 
@@ -37,30 +21,33 @@ function generateMarkdown(data) {
   
   ## Installation
   ${data.installation}
-  <p align="center">(<a href="#bringToTop">Bring to top of page</a>)</p>
+  
 
   ## Usage
   ${data.usage}
-  <p align="center">(<a href="#bringToTop">Bring to top of page</a>)</p>
+  
 
   ## Contributing
   ${data.contribution}
-  <p align="center">(<a href="#bringToTop">Bring to top of page</a>)</p>
+  
+
+  ## Credits
+  ${data.credits}
 
   ## Tests
   ${data.test}
-  <p align="center">(<a href="#bringToTop">Bring to top of page</a>)</p>
+  
 
   ## License
   Copycenter (c) [${data.username}](https://github.com/${data.username}). All centers reserved. 
   \nLicensed under the [${data.license} license](https://choosealicense.com/licenses/${data.license.toLowerCase()}/)
-  <p align="center">(<a href="#bringToTop">Bring to top of page</a>)</p>
+  
 
   ## Questions
   Feel free to contact me with questions or comments @:
   - GitHub: [${data.username}](https://github.com/${data.username})
   - Email: [${data.email}](mailto:${data.email})
-  <p align="center">(<a href="#bringToTop">Bring to top of page</a>)</p>
+  
   `;
   
 }
